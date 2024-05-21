eff1fe08b52aca583095a8030fd923d624c8206d
kooreung <crisishyun@gmail.com>
2024-05-21 10:12:55 +0900

    최초 커밋

---
+++ src/main/java/com/projectspring240521/ProjectSpring240521Application.java
@@ -0,0 +1,13 @@
+package com.projectspring240521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpring240521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpring240521Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectspring240521/ProjectSpring240521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectspring240521;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpring240521ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
