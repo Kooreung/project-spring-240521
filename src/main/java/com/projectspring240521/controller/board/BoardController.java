dc33587d63e62ba9b7606b2f42be60cf0753481a
kooreung <crisishyun@gmail.com>
2024-05-21 12:35:08 +0900

    글 작성 시 조건에 따라 400 코드 Return

--- src/main/java/com/projectspring240521/controller/board/BoardController.java
+++ src/main/java/com/projectspring240521/controller/board/BoardController.java
@@ -3,6 +3,7 @@ package com.projectspring240521.controller.board;
 import com.projectspring240521.domain.board.Board;
 import com.projectspring240521.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class BoardController {
 
     @PostMapping("add")
     public void add(@RequestBody Board board) {
-
-        service.add(board);
+        if (service.validate(board)) {
+            service.add(board);
+        } else {
+            ResponseEntity.badRequest().build();
+        }
     }
 }
