e55deac69f3a4d521988d202f60c93839d6634ff
kooreung <crisishyun@gmail.com>
2024-05-22 09:48:50 +0900

    회원가입 로직

--- sql/project.sql
+++ sql/project.sql
@@ -1,21 +1,23 @@
 USE project2;
 
-CREATE TABLE board (
-    id INT PRIMARY KEY AUTO_INCREMENT ,
-    title VARCHAR(100) NOT NULL ,
-    content VARCHAR(1000) NOT NULL ,
-    writer VARCHAR(100) NOT NULL ,
-    inserted DATETIME NOT NULL DEFAULT NOW()
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(100)  NOT NULL,
+    content  VARCHAR(1000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
 );
 
 SELECT *
 FROM board
-ORDER BY id DESC ;
+ORDER BY id DESC;
 
-CREATE TABLE member (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(50) NOT NULL UNIQUE ,
-    password VARCHAR(20) NOT NULL ,
-    nick_name VARCHAR(20) NOT NULL UNIQUE ,
-    inserted DATETIME NOT NULL DEFAULT NOW()
+CREATE TABLE member
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    email     VARCHAR(50) NOT NULL UNIQUE,
+    password  VARCHAR(20) NOT NULL,
+    nick_name VARCHAR(20) NOT NULL UNIQUE,
+    inserted  DATETIME    NOT NULL DEFAULT NOW()
 )
