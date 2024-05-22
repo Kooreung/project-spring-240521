086292889c55850851eb8b1d05d637ff26c0355e
kooreung <crisishyun@gmail.com>
2024-05-22 12:46:02 +0900

    암호 Encoding 저장 회원 가입 시 이메일 , 별명 의 공백을 제거

--- src/main/java/com/projectspring240521/service/member/MemberService.java
+++ src/main/java/com/projectspring240521/service/member/MemberService.java
@@ -3,6 +3,7 @@ package com.projectspring240521.service.member;
 import com.projectspring240521.domain.member.Member;
 import com.projectspring240521.mapper.member.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     final MemberMapper mapper;
+    final BCryptPasswordEncoder passwordEncoder;
 
     public void add(Member member) {
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        member.setEmail(member.getEmail().trim());
+        member.setNickName(member.getNickName().trim());
         mapper.insert(member);
     }
 
