64b25a8668ca8e31ca2ed1301d60dc7112bdd0bf
kooreung <crisishyun@gmail.com>
2024-05-23 15:27:03 +0900

    jwt 토큰 로그인 로그아웃

--- src/main/java/com/projectspring240521/service/member/MemberService.java
+++ src/main/java/com/projectspring240521/service/member/MemberService.java
@@ -4,10 +4,16 @@ import com.projectspring240521.domain.member.Member;
 import com.projectspring240521.mapper.member.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -15,6 +21,7 @@ import java.util.List;
 public class MemberService {
     final MemberMapper mapper;
     final BCryptPasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
 
     public void add(Member member) {
         member.setPassword(passwordEncoder.encode(member.getPassword()));
@@ -91,5 +98,31 @@ public class MemberService {
             return false;
         }
         return true;
+    }
+
+    public Map<String, Object> getToken(Member member) {
+        Map<String, Object> result = null;
+        Member db = mapper.selectByEmail(member.getEmail());
+        if (db != null) {
+            if (passwordEncoder.matches(member.getPassword(), db.getPassword())) {
+                result = new HashMap<>();
+                String token = "";
+                Instant now = Instant.now();
+                JwtClaimsSet claims = JwtClaimsSet.builder()
+                        .issuer("self")
+                        .issuedAt(now)
+                        .expiresAt(now.plusSeconds(60 * 60 * 24 * 7))
+                        .subject(member.getEmail())
+                        .claim("scope", "") // 권한
+                        .claim("nickName", db.getNickName())
+                        .build();
+
+                token = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+                result.put("token", token);
+            }
+        }
+
+        return result;
     }
 }
