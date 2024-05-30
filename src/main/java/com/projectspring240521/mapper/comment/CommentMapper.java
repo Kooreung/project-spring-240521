8758bd9038643895a21e5e095fb7df18de5c5487
kooreung <crisishyun@gmail.com>
2024-05-30 12:31:59 +0900

    댓글 목록 응답

--- src/main/java/com/projectspring240521/mapper/comment/CommentMapper.java
+++ src/main/java/com/projectspring240521/mapper/comment/CommentMapper.java
@@ -3,6 +3,9 @@ package com.projectspring240521.mapper.comment;
 import com.projectspring240521.domain.comment.Comment;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
@@ -13,4 +16,12 @@ public interface CommentMapper {
             VALUES (#{boardId}, #{memberId}, #{comment})
             """)
     int insert(Comment comment);
+
+    @Select("""
+            SELECT *
+            FROM comment
+            WHERE board_id = #{boardId}
+            ORDER BY id DESC 
+            """)
+    List<Comment> selectAllByBoardId(Integer boardId);
 }
