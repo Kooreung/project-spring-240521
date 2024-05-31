e2170b2277570e400708231e8439d9dbf43f467b
kooreung <crisishyun@gmail.com>
2024-05-31 11:14:59 +0900

    댓글 수정 완료 후 댓글 새로고침

--- src/main/java/com/projectspring240521/mapper/comment/CommentMapper.java
+++ src/main/java/com/projectspring240521/mapper/comment/CommentMapper.java
@@ -1,10 +1,7 @@
 package com.projectspring240521.mapper.comment;
 
 import com.projectspring240521.domain.comment.Comment;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -50,4 +47,11 @@ public interface CommentMapper {
             WHERE member_id = #{memberId}
             """)
     int deleteCommentByMemberId(Integer memberId);
+
+    @Update("""
+            UPDATE comment
+            SET comment = #{comment}
+            WHERE id = #{id}
+            """)
+    int update(Comment comment);
 }
--- src/main/java/com/projectspring240521/service/comment/CommentService.java
+++ src/main/java/com/projectspring240521/service/comment/CommentService.java
@@ -52,4 +52,8 @@ public class CommentService {
         }
         return true;
     }
+
+    public void update(Comment comment) {
+        mapper.update(comment);
+    }
 }
